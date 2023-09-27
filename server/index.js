90a111658f959dd906c24e756dd96cf1f6c824c2
@AbdulSalam <iabdulsalam635@gmail.com>
2023-09-27 23:16:15 +0000

    deployment changes

--- server/index.js
+++ server/index.js
@@ -6,16 +6,15 @@ const cors = require("cors");
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path= require('path')
 const adminRoute = require("./routes/admin");
 const depositorRoute = require("./routes/depositors");
 const subscribeRoute = require("./routes/subscribe");
 
-// config for dotenv
 dotenv.config();
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// Connect DB
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -28,10 +27,33 @@ mongoose
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => res.send("Hello from vault :)"));
 app.use("/api/adminRoute", adminRoute);
 app.use("/api/depositorRoute", depositorRoute);
 app.use("/api/subscribeRoute", subscribeRoute);
+
+// -------------------Deployment--------------------------
+
+const __dirname1=path.resolve()
+
+if (process.env.NODE_ENV==='production'){
+
+  app.use(express.static(path.join(__dirname1,'../client/build')))
+
+  app.get( '*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"client",'build','index.html'))
+  })
+
+} else{
+
+  app.get("/", (req, res) => res.send("Hello from vault :)"));
+
+
+}
+
+
+// -------------------Deployment--------------------------
+
+
 
 // Port setup
 const port = process.env.PORT || 5000;
